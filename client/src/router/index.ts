135bbaca7bc40969e4eb8b2a2aa6793febec58b9
SPinGH <salamatovpaul@yandex.ru>
2022-11-18 20:39:03 +0300

    Added protected route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -5,6 +5,7 @@ import ProductsPage from '@/pages/ProductsPage.vue';
 import SearchPage from '@/pages/SearchPage.vue';
 import MainPage from '@/pages/MainPage.vue';
 import AuthPage from '@/pages/AuthPage.vue';
+import store from '@/store';
 
 const routes: RouteRecordRaw[] = [
     {
@@ -41,6 +42,17 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
     linkActiveClass: 'active',
+});
+
+router.beforeEach((to, from, next) => {
+    if (
+        (to.meta.allowRole === 'USER' && store.state.auth.user?.role !== 'USER') ||
+        (to.meta.allowRole === 'ADMIN' && store.state.auth.user?.role !== 'ADMIN')
+    ) {
+        next(from.path);
+    } else {
+        next();
+    }
 });
 
 export default router;
